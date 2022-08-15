63068a6dc6996a25be82992601f196e8d1738f36
TFMahabub <rjmahabub543@gmail.com>
2022-08-15 18:02:15 +0600

    5th video

--- another.js
+++ another.js
@@ -15,5 +15,16 @@ document.getElementById('update-button').addEventListener('click', function(){
   const inputBoxValue = inputBox.value;
 
   const inputTitle = document.getElementById('input-title');
-  inputTitle.innerText= inputBoxValue;
+  inputTitle.innerText = inputBoxValue;
+})
+
+  //text-area-new comment-
+document.getElementById('submit-button').addEventListener('click', function(){
+  const textArea = document.getElementById('text-area');
+  const newComment = textArea.value;
+
+  const textContainer = document.getElementById('text-container');
+  newP = document.createElement('p');
+  newP.innerText = newComment;
+  textContainer.appendChild(newP);
 })
