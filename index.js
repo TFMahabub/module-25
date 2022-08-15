e02774c01dc3075d3f3aeb625d29c616217c1d23
TFMahabub <rjmahabub543@gmail.com>
2022-08-15 07:58:06 +0600

    2nd video

---
+++ index.js
@@ -0,0 +1,20 @@
+//yellow-button-
+const bgYellow = document.getElementById('yellow-button');
+bgYellow.onclick = function bgYellowOn(){
+  document.body.style.backgroundColor= 'yellow'
+}
+
+
+//red-button-
+const bgRed = document.getElementById('red-button');
+bgRed.onclick = bgRedOn;
+function bgRedOn(){
+  document.body.style.backgroundColor= 'red';
+}
+
+//purple-button-
+const bgPurple = document.getElementById('purple-button');
+bgPurple.onclick = bgPurpleOn;
+function bgPurpleOn(){
+  document.body.style.backgroundColor= 'purple';
+}
